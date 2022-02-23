65ee011a1e879e0444d2fffeb2bc09cb81959bda
Flor-M0nserrat <71458469+Flor-M0nserrat@users.noreply.github.com>
2022-02-23 15:21:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Welcome to you WebApp</title>
+    <title>Welcome to you WebApp UTBIS</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
